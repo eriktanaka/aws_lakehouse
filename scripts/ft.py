66bec1ba4f5747abaf4ff823885fae0425999e3c
Erik Tanaka <erik.ngk.tnk@gmail.com>
2026-05-25 20:19:44 -0300

    [FIX] ingestion endpoint

--- scripts/ft.py
+++ scripts/ft.py
@@ -1,32 +1,11 @@
-import time
-import sys
+from datetime import datetime, timedelta
 import requests
 import pandas as pd
-import awswrangler as wr
-from datetime import datetime, timedelta
-from awsglue.utils import getResolvedOptions
-
-args = getResolvedOptions(
-    sys.argv,
-    [
-        'bronze_bucket',
-        'bronze_database',
-    ]
-     )
-
-
-OUTPUT_BUCKET = args['bronze_bucket']
-PROJECT = "btc"
-OUTPUT_DATABASE = args['bronze_database']
-OUTPUT_TABLE_NAME = f"{PROJECT}"
-OUTPUT_S3_PATH = f"s3://{OUTPUT_BUCKET}/{PROJECT}/{OUTPUT_TABLE_NAME}/"
-TEMP_PATH = f"s3://{OUTPUT_BUCKET}/temp-athena/"
-
-
+import time
 SYMBOL = "BTCUSDT"
 INTERVAL = "1h"   
 LIMIT = 1000      
-ENDPOINT = "https://api.binance.us/api/v3/klines"
+ENDPOINT = "https://api.binance.com/api/v3/klines"
 
 def get_binance_data(symbol, interval, limit, days_back):
     print(f"Starting data extraction for {symbol} for the last {days_back} days...")
@@ -69,22 +48,3 @@ columns = [
 ]
 
 df = pd.DataFrame(raw_data, columns=columns)
-
-df = df.astype(str)
-
-df["ingestion_timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-df["symbol"] = SYMBOL
-
-print(f"Data extraction finished. {len(df)} records stored.")
-
-print("Storing data in s3 + athena...")
-wr.athena.to_iceberg(
-    df=df,
-    database=OUTPUT_DATABASE,
-    table=OUTPUT_TABLE_NAME,
-    table_location=OUTPUT_S3_PATH,
-    temp_path=TEMP_PATH,
-    mode="overwrite"
-)
-
-print("Bronze process finished")
